33232d2e519d5d2c17d2c3f740c1a3e125ace638
neethusobhish <neethuharidas.1111@gmail.com>
2026-06-28 06:47:14 +0530

    Add files via upload

--- Assignment3.py
+++ Assignment3.py
@@ -1,16 +1,15 @@
 
 
-text=input("Enter a string ")
-unique= True
+def check_unique(text):
+    for i in range(len(text)):
+        for j in range(i+1, len(text)):
+            if text[i]==text[j]:
+                return False
+    return True
 
-for i in range(len(text)):
-    for j in range(i+1, len(text)):
-        if text[i]==text[j]:
-            unique=False
-            break
-        if not unique:
-            break
-if unique:
+sting=input("Enter a string ")
+
+if check_unique(sting):
     print("True")
 else:
     print("False")
